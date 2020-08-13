aca72610d511faee222f1379c020b9b9cfdba658
jellymlg <39315532+jellymlg@users.noreply.github.com>
2020-08-13 18:11:17 +0200

    Add files via upload

---
+++ src/main/java/net/mod/Main.java
@@ -0,0 +1,21 @@
+package net.mod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Main implements ModInitializer {
+	public static final Block XD = new Block(FabricBlockSettings.of(Material.METAL).breakByHand(true).sounds(BlockSoundGroup.BAMBOO).velocityMultiplier(3));
+	@Override
+	public void onInitialize() {
+		Registry.register(Registry.BLOCK, new Identifier("mod:xd"), XD);
+		Registry.register(Registry.ITEM, new Identifier("mod:xd"), new BlockItem(XD, new Item.Settings().group(ItemGroup.MISC)));
+	}
+}
