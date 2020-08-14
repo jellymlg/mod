7e47c43886ed94963041268191383dd75bbcffa8
BuildTools <unconfigured@null.spigotmc.org>
2020-08-14 18:21:06 +0200

    imrek icon

--- src/main/java/net/mod/Main.java
+++ src/main/java/net/mod/Main.java
@@ -9,19 +9,19 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
-	public static final ItemGroup ITEMS = FabricItemGroupBuilder.create(new Identifier("mod:items")).icon(() -> new ItemStack(Items.BEDROCK)).build();
-	public static final Item IMREK = new Item(new Item.Settings().group(ITEMS));
+	public static Item IMREK = new Item(new Item.Settings());
+	public static final ItemGroup ITEMS = FabricItemGroupBuilder.create(new Identifier("mod:items")).icon(() -> new ItemStack(IMREK)).build();
 	public static final Block XD = new Block(FabricBlockSettings.of(Material.METAL).breakByHand(true).sounds(BlockSoundGroup.BAMBOO).velocityMultiplier(3));
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK, new Identifier("mod:xd"), XD);
+		IMREK = new Item(new Item.Settings().group(ITEMS));
 		Registry.register(Registry.ITEM, new Identifier("mod:imrek"), IMREK);
+		Registry.register(Registry.BLOCK, new Identifier("mod:xd"), XD);
 		Registry.register(Registry.ITEM, new Identifier("mod:xd"), new BlockItem(XD, new Item.Settings().group(ITEMS)));
 	}
 }
