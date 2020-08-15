cb23df0fb8bf327cbd195c4271be278202e583ed
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 14:46:37 +0200

    add crop

--- src/main/java/net/mod/Main.java
+++ src/main/java/net/mod/Main.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -18,6 +19,7 @@ public class Main implements ModInitializer {
 	public static final ItemGroup ITEMS = FabricItemGroupBuilder.create(ID("items")).icon(() -> new ItemStack(IMREK)).build();
 	public static final Block XD = new Block(FabricBlockSettings.of(Material.METAL).breakByHand(true).sounds(BlockSoundGroup.HONEY).velocityMultiplier(3));
 	public static final Item SCYTHE = new Scythe(new Item.Settings().maxDamage(100).group(ITEMS));
+	public static final Block CROP = new Crop(FabricBlockSettings.copy(Blocks.WHEAT));
 	@Override
 	public void onInitialize() {
 		IMREK = new Item(new Item.Settings().group(ITEMS));
@@ -25,6 +27,8 @@ public class Main implements ModInitializer {
 		Registry.register(Registry.BLOCK, ID("xd"), XD);
 		Registry.register(Registry.ITEM, ID("xd"), new BlockItem(XD, new Item.Settings().group(ITEMS)));
 		Registry.register(Registry.ITEM, ID("scythe"), SCYTHE);
+		Registry.register(Registry.BLOCK, ID("crop"), CROP);
+		Registry.register(Registry.ITEM, ID("crop"), new BlockItem(CROP, new Item.Settings().group(ITEMS)));
 	}
 	private static Identifier ID(String itemName) {
 		return new Identifier("mod", itemName);
