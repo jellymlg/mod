0db3a27e3159d151a3c9f22d0a08a276aab1e12b
Panta Erik <erikpanta@gmail.com>
2020-08-16 21:39:07 +0200

    scythe functions

--- src/main/java/net/mod/Crop.java
+++ src/main/java/net/mod/Crop.java
@@ -1,16 +1,13 @@
 package net.mod;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
 public class Crop extends CropBlock {
@@ -23,17 +20,13 @@ public class Crop extends CropBlock {
     }
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if(this.isMature(state) && player.getMainHandStack().getItem() == Registry.ITEM.get(Main.ID("scythe"))) {
-            Block.dropStack(world, pos, new ItemStack(Items.WHEAT, 5));
-            world.setBlockState(pos, this.withAge(0), 2);
-        }
         if(!this.isMature(state) && player.getMainHandStack().getItem() == Items.BONE_MEAL) {
             this.applyGrowth(world, pos, state);
             if(!player.abilities.creativeMode) {
                 player.getMainHandStack().decrement(1);
             }
         }
-        return ActionResult.SUCCESS;
+        return ActionResult.PASS;
     }
     @Override
     protected int getGrowthAmount(World world) {
--- src/main/java/net/mod/Scythe.java
+++ src/main/java/net/mod/Scythe.java
@@ -1,9 +1,32 @@
 package net.mod;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
 
 public class Scythe extends Item {
     public Scythe(Settings settings) {
         super(settings);
+    }
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        ItemStack tool = context.getStack();
+        BlockState state = context.getWorld().getBlockState(context.getBlockPos());
+        if(state.getBlock().is(Main.CROP) && ((Crop) state.getBlock()).isMature(state)) {
+            Block.dropStack(context.getWorld(), context.getBlockPos(), new ItemStack(Main.WHEATSTICK, 5));
+            context.getWorld().setBlockState(context.getBlockPos(), ((Crop) state.getBlock()).withAge(0), 2);
+            if(!context.getPlayer().isCreative()) {
+                tool.setDamage(tool.getDamage() + 1);
+            }
+            if(tool.getDamage() > tool.getMaxDamage()) {
+                context.getPlayer().inventory.removeOne(tool);
+                context.getPlayer().playSound(SoundEvents.ITEM_SHIELD_BREAK, 1.0f, 1.0f);
+            }
+        }
+        return ActionResult.PASS;
     }
 }
