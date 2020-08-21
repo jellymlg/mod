2459aad2e1db5cda5b7047ffab330a5fad10a01a
Panta Erik <erikpanta@gmail.com>
2020-08-21 22:11:43 +0200

    no particles

--- src/main/java/net/mod/Imrek.java
+++ src/main/java/net/mod/Imrek.java
@@ -4,7 +4,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.registry.Registry;
@@ -19,7 +18,6 @@ public class Imrek extends Item {
         user.playSound(Registry.SOUND_EVENT.get((int) (Math.random() * Registry.SOUND_EVENT.getIds().size())), 1.0f, 1.0f);
         MinecraftClient client = MinecraftClient.getInstance();
         client.gameRenderer.showFloatingItem(new ItemStack(this));
-        client.particleManager.addEmitter(user, ParticleTypes.TOTEM_OF_UNDYING, 30);
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
 }
