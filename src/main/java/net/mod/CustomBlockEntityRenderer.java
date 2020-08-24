56c43a2bfd5143f8af07ab23c7506f55c0286c76
BuildTools <unconfigured@null.spigotmc.org>
2020-08-24 21:01:06 +0200

    countertop functions (save load fix)

--- src/main/java/net/mod/CustomBlockEntityRenderer.java
+++ src/main/java/net/mod/CustomBlockEntityRenderer.java
@@ -15,10 +15,10 @@ public class CustomBlockEntityRenderer extends BlockEntityRenderer<CountertopEnt
     @Override
     public void render(CountertopEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
-        if(!entity.item.isEmpty()) {
+        if(!entity.isEmpty()) {
             matrices.translate(0.5D, 1.0D, 0.5D);
             int lightAbove = WorldRenderer.getLightmapCoordinates(entity.getWorld(), entity.getPos().up());
-            MinecraftClient.getInstance().getItemRenderer().renderItem(entity.item, ModelTransformation.Mode.GROUND, lightAbove, overlay, matrices, vertexConsumers);
+            MinecraftClient.getInstance().getItemRenderer().renderItem(entity.getStack(0), ModelTransformation.Mode.GROUND, lightAbove, overlay, matrices, vertexConsumers);
         }
         matrices.pop();
     }
---
+++ src/main/java/net/mod/SingletInventory.java
@@ -0,0 +1,55 @@
+package net.mod;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+public interface SingletInventory extends Inventory {
+    DefaultedList<ItemStack> getItems();
+    @Override
+    default int size() {
+        return 1;
+    }
+    @Override
+    default boolean isEmpty() {
+        for(int i = 0; i < size(); i++) {
+            if(!getStack(i).isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    default ItemStack getStack(int slot) {
+        return getItems().get(slot);
+    }
+    @Override
+    default ItemStack removeStack(int slot, int amount) {
+        ItemStack removed = Inventories.splitStack(getItems(), slot, amount);
+        if(!removed.isEmpty()) {
+            markDirty();
+        }
+        return removed;
+    }
+    @Override
+    default ItemStack removeStack(int slot) {
+        return Inventories.removeStack(getItems(), slot);
+    }
+    @Override
+    default void setStack(int slot, ItemStack stack) {
+        getItems().set(slot, stack);
+        if(stack.getCount() > getMaxCountPerStack()) {
+            stack.setCount(getMaxCountPerStack());
+        }
+    }
+    @Override
+    default void clear() {
+        getItems().clear();
+    }
+    @Override
+    default boolean canPlayerUse(PlayerEntity player) {
+        return false;
+    }
+}
