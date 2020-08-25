1eb3840707d382aadea6ee55ffce79a5bf12253e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 22:55:42 +0200

    countertop directional items

--- src/main/java/net/mod/CustomBlockEntityRenderer.java
+++ src/main/java/net/mod/CustomBlockEntityRenderer.java
@@ -7,9 +7,12 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Quaternion;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 
 public class CustomBlockEntityRenderer extends BlockEntityRenderer<CountertopEntity> {
+    private Direction q;
+    private static final double Y_OFFSET = 0.95;
     public CustomBlockEntityRenderer(BlockEntityRenderDispatcher dispatcher) {
         super(dispatcher);
     }
@@ -17,12 +20,31 @@ public class CustomBlockEntityRenderer extends BlockEntityRenderer<CountertopEnt
     public void render(CountertopEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
         if(!entity.isEmpty()) {
-            matrices.translate(0.5D, 0.99D, 0.3D);
-            matrices.multiply(new Quaternion(90.0f, 0.0f, 0.0f, true));
+            q = entity.getCachedState().get(Properties.HORIZONTAL_FACING);
+            directionalTranslation(matrices, q);
+            matrices.multiply(q.getOpposite().getRotationQuaternion());
             matrices.scale(1.5f, 1.5f, 1.5f);
             int lightAbove = WorldRenderer.getLightmapCoordinates(entity.getWorld(), entity.getPos().up());
             MinecraftClient.getInstance().getItemRenderer().renderItem(entity.getStack(0), ModelTransformation.Mode.GROUND, lightAbove, overlay, matrices, vertexConsumers);
         }
         matrices.pop();
+    }
+    private void directionalTranslation(MatrixStack matrix, Direction dir) {
+        switch(dir) {
+            case NORTH:
+                matrix.translate(0.5D, Y_OFFSET, 0.3D);
+                break;
+            case EAST:
+                matrix.translate(0.7D, Y_OFFSET, 0.5D);
+                break;
+            case SOUTH:
+                matrix.translate(0.5D, Y_OFFSET, 0.7D);
+                break;
+            case WEST:
+                matrix.translate(0.3D, Y_OFFSET, 0.5D);
+                break;
+            case DOWN:break;
+            case UP:break;
+        }
     }
 }
